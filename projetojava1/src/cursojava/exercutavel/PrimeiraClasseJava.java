48620aaff71d0e1fc5cc89ffbc77f25e77cf3444
Gustavo Lima Rocha de Sousa <gustavolrs18@gmail.com>
2021-09-06 05:55:24 -0300

    Usando Double

--- projetojava1/src/cursojava/exercutavel/PrimeiraClasseJava.java
+++ projetojava1/src/cursojava/exercutavel/PrimeiraClasseJava.java
@@ -10,11 +10,13 @@ public class PrimeiraClasseJava {
 		System.out.println("Java é orientado a objetos");
 		
 		/*Notas aprendendo a lidar com números*/
-		int nota1 = 90;
-		int nota2 = 70;
-		int nota3 = 50;
-		int nota4 = 88;
-		System.out.println("Media das notas = " + (nota1 + nota2 + nota3 + nota4) /4);
+		double nota1 = 90;
+		double nota2 = 70;
+		double nota3 = 50;
+		double nota4 = 88;
+		double mediaFinal = (nota1 + nota2 + nota3 + nota4) /4;
+		
+		System.out.println("Media das notas = " + mediaFinal);
 	}
 	
 }
