567fa2e978c0dd8d10778d7b80dc8804489b6134
Gustavo Lima Rocha de Sousa <gustavolrs18@gmail.com>
2021-10-01 11:24:02 -0300

    Esquemas de carros + Esquemas de Notas.

--- projetojava1/src/cursojava/exercutavel/PrimeiraClasseJava.java
+++ projetojava1/src/cursojava/exercutavel/PrimeiraClasseJava.java
@@ -151,7 +151,7 @@ public class PrimeiraClasseJava {
 		System.out.println("Sobraram exatament: " + resto + " Carros no estoque." );*/
 		/* Entrada de dados*/
 		
-		/* Entrada de dados*/
+		/* Entrada de dados Esquema de Carros*/
 		
 		String carros = JOptionPane.showInputDialog("Informe a Quantidade de carros?");
 		String pessoas = JOptionPane.showInputDialog("Informe a Quantidade de pessoas?");
@@ -172,6 +172,30 @@ public class PrimeiraClasseJava {
 			JOptionPane.showMessageDialog(null, "O resto da divisão é " + resto);
 				}
 			}
+			/* Entrada de dados Esquema de Notas*/
+			String nota1 = JOptionPane.showInputDialog("Informe a Nota 1 ");
+			String nota2 = JOptionPane.showInputDialog("Informe a Nota 2 ");
+			String nota3 = JOptionPane.showInputDialog("Informe a Nota 3 ");
+			String nota4 = JOptionPane.showInputDialog("Informe a Nota 4 ");
+			
+			double dNota1 = Double.parseDouble(nota1);
+			double dNota2 = Double.parseDouble(nota2);
+			double dNota3 = Double.parseDouble(nota3);
+			double dNota4 = Double.parseDouble(nota4);
+			
+			double media = (dNota1 + dNota2 + dNota3 + dNota4) /4;
+			
+			if(media >= 50) {
+				
+				if(media >=70){
+					JOptionPane.showMessageDialog(null, "Aluno Está Aprovado com média de : " + media);
+				}else {
+			JOptionPane.showMessageDialog(null, "Aluno em recuperação com média de : " + media);
+				}
+			}else {
+				JOptionPane.showMessageDialog(null, "Aluno está reprovado com média de : " + media);
+			}	
+				
 	}	
 		
 }
